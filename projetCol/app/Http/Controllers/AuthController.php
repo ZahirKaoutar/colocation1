978748ac072b1c8bbe90f44768a5e4b9c71c822f
KAWKAW <zahirkaoutar2003@gmail.com>
2026-02-25 21:32:54 +0000

    creation controller auth avec method login register avce validation dans dossier Request

--- projetCol/app/Http/Controllers/AuthController.php
+++ projetCol/app/Http/Controllers/AuthController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Requests\RegisterRequest;
 
 use Illuminate\Http\Request;
 
@@ -9,18 +10,45 @@ class AuthController extends Controller
  public function Login(){
     return view('auth.login');
  }
- public function SubmitLogin(){
+ public function SubmitLogin(LoginRequest $request){
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+
+
+        if (auth()->user()->banned_at) {
+            Auth::logout();
+            return back()->withErrors(['email' => 'Votre compte est banni']);
+        }
+
+        return redirect()->route('dashboard');
+    }
+
+    return back()->withErrors([
+        'email' => 'Email ou mot de passe incorrect'
+    ]);
 
  }
  public function Register(){
     return view('auth.Register');
  }
- public function RegisterSubmit(){
-    // $user=User::create([
-    //     'name'=>$request->name,
-    //     'email'=>$request->email,
-    //     ''
-//     ])
-//  }
+ public function RegisterSubmit(RegisterRequest $request){
+
+  $role=User::count()===0?'admin':'user';
+     $user=User::create([
+    'name'=>$request->name,
+       'email'=>$request->email,
+        'password'=>Hash::check($request->password),
+        'role'=>$role,
+        'reputation'=>0,
+        'banned_at'=>null,
+     ]);
+
+   Auth::login($user);
+   return redirect()->route('dashboard');
+
+
+  }
+
  }
-}
+
